db93f877567ce5d320d8ad73ec8be6c1a9c71a5b
Andrew Kim <ndbkim@gmail.com>
2025-12-15 02:17:33 -0800

    added death floor

--- supercattoball/stage/death_floor/death_floor.gd
+++ supercattoball/stage/death_floor/death_floor.gd
@@ -2,6 +2,6 @@ extends Area3D
 
 
 
-#func _on_body_entered(body: CattoBall) -> void:
-	#if body is CattoBall:
-		#print("DEATH")
+func _on_body_entered(body: CattoBall) -> void:
+	if body:
+		print("DEATH")
