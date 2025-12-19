61d7c549d0d89332a90fcae4f6c1616a0916245c
Andrew Kim <ndbkim@gmail.com>
2025-12-18 19:31:00 -0800

    added so death floor invokes continue screen and updates lives

--- supercattoball/stage/death_floor/death_floor.gd
+++ supercattoball/stage/death_floor/death_floor.gd
@@ -3,6 +3,8 @@ extends Area3D
 @export var catto_spawner : Marker3D
 @onready var debug_shape: MeshInstance3D = $MeshInstance3D
 
+const continue_screen : PackedScene = preload("res://menus/continue_menu/continue_screen.tscn")
+
 func _ready() -> void:
 	debug_shape.hide()
 
@@ -12,3 +14,9 @@ func _on_body_entered(body: CattoBall) -> void:
 		body.global_transform = catto_spawner.global_transform
 		## TODO set process false until after countdown is reinvoked
 		body.linear_velocity = Vector3.ZERO
+		GameManager.total_lives -= 1
+		
+		if GameManager.total_lives <= 0:
+			get_tree().change_scene_to_packed.call_deferred(continue_screen)
+		else:
+			GameManager.update_lives.emit()
