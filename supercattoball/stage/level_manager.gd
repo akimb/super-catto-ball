99bc6efd1d358ca6f997b7617350f0a5d1e6afa9
Andrew Kim <ndbkim@gmail.com>
2025-12-18 19:28:56 -0800

    make level manager invoke level buffer between loading

--- supercattoball/stage/level_manager.gd
+++ supercattoball/stage/level_manager.gd
@@ -3,10 +3,13 @@ extends Control
 @onready var skybox_camera_3d: Camera3D = $SkyboxViewport/SkyboxCamera3D
 @onready var game_viewport: SubViewport = $GameViewport
 
+const ready_buffer_screen : PackedScene = preload("res://menus/level_ready_screen/level_ready_buffer_screen.tscn")
 
 func _ready() -> void:
 	GameManager.change_level.connect(_on_level_change)
 	GameManager.update_floor.emit(GameManager.current_level)
+	var buffer_screen = ready_buffer_screen.instantiate()
+	add_child(buffer_screen)
 	#game_viewport.get_child(0).set_process(false) ## TODO this is to prevent input until after the "Ready, GO!" is played
 
 func _input(event: InputEvent) -> void:
@@ -14,8 +17,8 @@ func _input(event: InputEvent) -> void:
 
 
 func _process(_delta: float) -> void:
-	if get_tree().get_first_node_in_group("CattoBall"):
-		skybox_camera_3d.global_transform = get_tree().get_first_node_in_group("CattoBall").camera_pivot.transform
+	if get_tree().get_first_node_in_group("catto_ball"):
+		skybox_camera_3d.global_transform = get_tree().get_first_node_in_group("catto_ball").camera_pivot.transform
 
 func _on_level_change(level_index : int) -> void:
 	_do_level_change.call_deferred(level_index)
@@ -29,4 +32,6 @@ func _do_level_change(level_index : int) -> void:
 	var packed_scene : PackedScene = load(next_level_path)
 	var next_level : Level = packed_scene.instantiate()
 	game_viewport.add_child(next_level)
+	var buffer_screen = ready_buffer_screen.instantiate()
+	add_child(buffer_screen)
 	GameManager.update_floor.emit(level_index)
