131accccf3c99d945320af7dd163b494e255d0f4
Andrew Kim <ndbkim@gmail.com>
2025-12-20 22:17:13 -0800

    using embers's cat model

--- supercattoball/catto_ball/catto_ball.gd
+++ supercattoball/catto_ball/catto_ball.gd
@@ -1,11 +1,10 @@
 class_name CattoBall extends RigidBody3D
 
 @onready var camera_rig: Node3D = $CameraYaw/CameraRig
-#@onready var catto_model: Node3D = $Catto
 @onready var catto_model: Node3D = $catto
-#@onready var catto_model: Node3D = $cat_model
 @onready var camera_pivot: Node3D = $CameraYaw/CameraRig/CameraPivot
 @onready var pickup_location: Marker3D = $"CameraYaw/CameraRig/CameraPivot/Camera3D/Pickup Location"
+@onready var camera: Camera3D = $CameraYaw/CameraRig/CameraPivot/Camera3D
 
 
 func _physics_process(_delta: float) -> void:
