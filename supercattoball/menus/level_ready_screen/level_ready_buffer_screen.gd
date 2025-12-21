5e2078f443d8e57a959b4a2e851735ff001d0b98
Andrew Kim <ndbkim@gmail.com>
2025-12-20 16:26:39 -0800

    cleaned up buffer script and removed comments, added a buffer_finished signal

--- supercattoball/menus/level_ready_screen/level_ready_buffer_screen.gd
+++ supercattoball/menus/level_ready_screen/level_ready_buffer_screen.gd
@@ -1,36 +1,6 @@
-#extends Control
-#
-#signal finished
-#
-#@onready var ready_label: Label = $Ready
-#@onready var go_label: Label = $Go
-#
-#func _ready() -> void:
-	#set_anchors_preset(PRESET_FULL_RECT)
-	#_ready_sequence()
-#
-#func _ready_sequence() -> void:
-	#ready_label.modulate.a = 0
-	#go_label.modulate.a = 0
-#
-	#ready_label.show()
-	#var t := create_tween()
-	#t.tween_property(ready_label, "modulate:a", 1.0, 0.4)
-	#await t.finished
-	#await get_tree().create_timer(0.8).timeout
-	#ready_label.hide()
-#
-	#go_label.show()
-	#t = create_tween()
-	#t.tween_property(go_label, "modulate:a", 1.0, 0.4)
-	#await t.finished
-	#await get_tree().create_timer(0.8).timeout
-	#go_label.hide()
-#
-	#emit_signal("finished")
-	#queue_free()
-
 extends Control
+
+signal buffer_finished
 
 @onready var ready_label: Label = $Ready
 @onready var go_label: Label = $Go
@@ -38,37 +8,21 @@ extends Control
 const transparent : Color = Color("ffffff00")
 
 func _ready() -> void:
-	print("level buffer instantiation")
 	set_anchors_preset(PRESET_FULL_RECT)
 	play_buffer()
 
 func play_buffer() -> void:
 	var level : LevelManager = get_parent()
-	#get_tree().get_first_node_in_group("level_group").set_physics_process(false)
-	#get_tree().get_first_node_in_group("level_group").process_mode = Node.PROCESS_MODE_DISABLED
 	level.set_process_input(false)
-	#var level : LevelManager = get_parent() # grab the level manager
-	#
-	#await level.ready
-	#var level_group = level.game_viewport.get_child(0)
-	#var level_group: Node = null
-	#while not level_group:
-		#level_group = get_tree().get_first_node_in_group("level_group")
-		#await get_tree().process_frame
-		
-	#level_group.set_physics_process(false)
-	#level.set_process_input(false)
-	#if level:
-		#get_tree().get_first_node_in_group("level_group").set_physics_process(false)
-		#level.set_process_input(false)
 
 	ready_label.show()
 	go_label.show()
 
 	ready_label.modulate.a = 0.0
 	go_label.modulate.a = 0.0
-
+	
 	# READY fade in
+	AudioBus.game_ready.play()
 	var t := get_tree().create_tween()
 	t.tween_property(ready_label, "modulate:a", 1.0, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	await t.finished
@@ -76,6 +30,7 @@ func play_buffer() -> void:
 	ready_label.hide()
 
 	# GO fade in (new tween!)
+	AudioBus.go.play()
 	t = get_tree().create_tween()
 	t.tween_property(go_label, "modulate:a", 1.0, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	await t.finished
@@ -83,7 +38,7 @@ func play_buffer() -> void:
 	go_label.hide()
 
 	get_tree().get_first_node_in_group("level_group").set_physics_process(true)
-	#get_tree().get_first_node_in_group("level_group").process_mode = Node.PROCESS_MODE_INHERIT
 	level.set_process_input(true)
-
+	
+	buffer_finished.emit()
 	queue_free()
